ebd5509134c60dd45de22279f7b20b10619f3e38
SouravB10 <souravbehuria10@gmail.com>
2025-06-26 15:17:27 +0530

    screens

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -4,26 +4,24 @@ import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 type Props = {
-title: string;
+    title: string;
 };
 
 export default function CustomHeader({ title }: Props) {
-const router = useRouter();
-const bgColor = useColorModeValue('white', 'coolGray.900');
-const textColor = useColorModeValue('black', 'white');
+    const router = useRouter();
+    const bgColor = useColorModeValue('white', 'coolGray.900');
+    const textColor = useColorModeValue('black', 'white');
 
-return (
-<SafeAreaView edges={['top']} style={{ backgroundColor: bgColor }}>
-<HStack alignItems="center" px={3} borderBottomWidth={1} borderColor="coolGray.200" bg={bgColor} >
-<IconButton
-icon={<Icon as={Ionicons} name="arrow-back" size="md" color={textColor} />}
-onPress={() => router.back()}
-variant="ghost"
-/>
-<Text fontSize="lg" fontWeight="bold" ml={2} color={textColor}>
-{title}
-</Text>
-</HStack>
-</SafeAreaView>
-);
+    return (
+            <HStack alignItems="center" py={3}  px={1} borderBottomWidth={1} borderColor="coolGray.200" bg={bgColor} >
+                <IconButton
+                    icon={<Icon as={Ionicons} name="arrow-back" size="md" color={textColor} />}
+                    onPress={() => router.back()}
+                    variant="ghost"
+                />
+                <Text fontSize="lg" fontWeight="bold" ml={2} color={textColor}>
+                    {title}
+                </Text>
+            </HStack>
+    );
 }
