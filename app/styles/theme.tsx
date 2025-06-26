6e91a06c63768be849fcfdb06b2290a718685408
Naveen Nagam <naveennagam6015@gmail.com>
2025-06-26 15:18:22 +0530

    details page

---
+++ app/styles/theme.tsx
@@ -0,0 +1,18 @@
+
+
+export const color = {
+  primary: "#725CAD",
+  secondary: "#A2AADB",
+  success: "#418161",
+  fontcolor: "#283F45",
+  white: "#FFFFFF",
+  lightBlue: "#ADD8E6",
+  navyBlue: "#033c96",
+  blue: "#dfe0e8",
+  neutral: {
+    100: "#E8E8E8",
+    200: "#D9D9D9",
+    300: "#BFBFBF",
+    500: "#969696",
+  },
+};
